4e23b781bcd5962c3089c9935639fae1c56520f3
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2024-06-23 09:28:49 +0530

    Create Print Bracket Number.py
    Given a string str, the task is to find the bracket numbers, i.e., for each bracket in str, return i if the bracket is the ith opening or closing bracket to appear in the string. 
    
     Examples:
    
    Input:  str = "(aa(bdc))p(dee)"
    Output: 1 2 2 1 3 3
    Explanation: The highlighted brackets in
    the given string (aa(bdc))p(dee) are
    assigned the numbers as: 1 2 2 1 3 3.
    Input:  str = "(((()("
    Output: 1 2 3 4 4 5
    Explanation: The highlighted brackets in
    the given string (((()( are assigned
    the numbers as: 1 2 3 4 4 5
    Expected Time Complexity: O(|str|)
    Expected Auxiliary Space: O(|str|)
    
    Constraints:
    1 <= |str| <= 105
    str contains lowercase English alphabets, and '(', ')' characters
    At any index, the number of opening brackets is greater than or equal to closing brackets

---
+++ Print Bracket Number.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+class Solution:
+	def bracketNumbers(self, str):
+	    stack=[] 
+	    c=0
+	    ans=[]
+	    for i in range(len(str)):
+	        if(str[i]=='('):
+	            c+=1 
+	            ans.append(c)
+	            stack.append(c)
+	        elif(str[i]==')'):
+	            ans.append(stack[-1]) 
+	            stack.pop() 
+	    return ans
