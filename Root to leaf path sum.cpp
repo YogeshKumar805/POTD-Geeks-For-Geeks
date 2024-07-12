78806bff42d2b378a8e72f9431647b694d35e660
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2024-07-12 23:17:24 +0530

    Create Root to leaf path sum.cpp
    Given a binary tree and an integer target, check whether there is a root-to-leaf path with its sum as target.
    
    Examples :
    
    Input: tree = 1, target = 2
                /   \
              2     3
    Output: false
    Explanation: There is no root to leaf path with sum 2.
    Input: tree = 1,  target = 4
                /   \
              2     3
    Output: true
    Explanation: The sum of path from leaf node 3 to root 1 is 4.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(height of tree)
    
    Constraints:
    1 ≤ number of nodes ≤ 104
    1 ≤ target ≤ 106

---
+++ Root to leaf path sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool hasPathSum(Node *root, int target) {
+        // Your code here
+        if (root == NULL)
+            return false;
+        if (root->data == target && root->left == NULL && root->right == NULL)
+            return true;
+        return hasPathSum(root->left, target - root->data) ||
+               hasPathSum(root->right, target - root->data);
+    }
+};
