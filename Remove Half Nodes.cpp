6d9b518b63c2fab50e49891bd3844ab0b60b187a
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2024-07-20 20:48:43 +0530

    Create Remove Half Nodes.cpp
    You are given a binary tree and you need to remove all the half nodes (which have only one child). Return the root node of the modified tree after removing all the half-nodes.
    
    Note: The output will be judged by the inorder traversal of the resultant tree, inside the driver code.
    
    Examples:
    
    Input: tree = 5
                /   \
              7     8
            / 
          2
    Output: 2 5 8
    Explanation: In the above tree, the node 7 has only single child. After removing the node the tree becomes  2<-5->8. Hence, the answer is 2 5 8 & it is in inorder traversal.
    Input:  tree = 2   
                  / \   
                7   5 
    Output: 7 2 5
    Explanation: Here there are no nodes which has only one child. So the tree remains same.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(height of the binary tree)
    
    Constraints:
    1<=number of nodes<=104

---
+++ Remove Half Nodes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    Node* helper(Node* node){
+        if(!node) return nullptr;
+        if(!node->left && !node->right) return node;
+        
+        node->left = helper(node->left);
+        node->right = helper(node->right);
+        
+        if((node->left != nullptr && node->right != nullptr)) return node;
+        
+        if(!node->left){
+            return node->right;
+        }
+        if(!node->right){
+            return node->left;
+        }
+    }
+    Node *RemoveHalfNodes(Node *root) {
+        // code here
+        return helper(root);
+        // return root;
+    }
+};
+
