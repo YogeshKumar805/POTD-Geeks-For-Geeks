608150a9afb96ef722f2ea01cc55c970197cfe2d
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2024-12-03 14:03:11 +0530

    Create Min Chars to Add for Palindrome.java
    Given a string s, the task is to find the minimum characters to be added at the front to make the string palindrome.
    
    Note: A palindrome string is a sequence of characters that reads the same forward and backward.
    
    Examples:
    
    Input: s = "abc"
    Output: 2
    Explanation: Add 'b' and 'c' at front of above string to make it palindrome : "cbabc"
    Input: s = "aacecaaaa"
    Output: 2
    Explanation: Add 2 a's at front of above string to make it palindrome : "aaaacecaaaa"
    Constraints:
    1 <= s.size() <= 106

---
+++ Min Chars to Add for Palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int minChar(String A) {
+        String rev = new StringBuilder(A).reverse().toString();
+        String concat = A + "$" + rev;
+        int[] lps = new int[concat.length()];
+        int y = 0;
+        for (int x = 1; x < concat.length(); x++) {
+            while (y > 0 && concat.charAt(x) != concat.charAt(y)) {
+                y = lps[y - 1];
+            }
+            if (concat.charAt(x) == concat.charAt(y)) {
+                y++;
+            }
+            lps[x] = y;
+        }
+        return A.length() - lps[concat.length() - 1];
+    }
+}
