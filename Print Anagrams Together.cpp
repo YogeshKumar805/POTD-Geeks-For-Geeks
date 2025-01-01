8f2bb6fdc10d356b364385fde6c53bde17b08529
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2025-01-01 09:45:36 +0530

    Create Print Anagrams Together.cpp
    Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the original array. Look at the sample case for clarification.
    
    Note: The final output will be in lexicographic order.
    
    Examples:
    
    Input: arr[] = ["act", "god", "cat", "dog", "tac"]
    Output: [["act", "cat", "tac"], ["god", "dog"]]
    Explanation: There are 2 groups of anagrams "god", "dog" make group 1. "act", "cat", "tac" make group 2.
    Input: arr[] = ["no", "on", "is"]
    Output: [["is"], ["no", "on"]]
    Explanation: There are 2 groups of anagrams "is" makes group 1. "no", "on" make group 2.
    Input: arr[] = ["listen", "silent", "enlist", "abc", "cab", "bac", "rat", "tar", "art"]
    Output: [["abc", "cab", "bac"], ["listen", "silent", "enlist"], ["rat", "tar", "art"]]
    Explanation: 
    Group 1: "abc", "bac", and "cab" are anagrams.
    Group 2: "listen", "silent", and "enlist" are anagrams.
    Group 3: "rat", "tar", and "art" are anagrams.
    Constraints:
    1<= arr.size() <=100
    1<= arr[i].size() <=10

---
+++ Print Anagrams Together.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(int i = 0 ; i<arr.size() ; i++){
+            string s = arr[i];
+            sort(s.begin() , s.end());
+            mp[s].push_back(arr[i]);
+        }
+        
+        for(auto c:mp){
+            if(c.second.size()>0)
+            ans.push_back(c.second);
+        }
+        return ans;
+        
+    }
+};
