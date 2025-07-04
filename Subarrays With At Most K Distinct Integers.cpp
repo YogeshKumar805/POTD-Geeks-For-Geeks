3caa2c4d16efd422e2513688fe63fadf4a18b688
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2025-07-04 08:36:24 +0530

    Create Subarrays With At Most K Distinct Integers.cpp
    You are given an array arr[] of positive integers and an integer k, find the number of subarrays in arr[] where the count of distinct integers is at most k.
    
    Note: A subarray is a contiguous part of an array.
    
    Examples:
    
    Input: arr[] = [1, 2, 2, 3], k = 2
    Output: 9
    Explanation: Subarrays with at most 2 distinct elements are: [1], [2], [2], [3], [1, 2], [2, 2], [2, 3], [1, 2, 2] and [2, 2, 3].
    Input: arr[] = [1, 1, 1], k = 1
    Output: 6
    Explanation: Subarrays with at most 1 distinct element are: [1], [1], [1], [1, 1], [1, 1] and [1, 1, 1].
    Input: arr[] = [1, 2, 1, 1, 3, 3, 4, 2, 1], k = 2
    Output: 24
    Explanation: There are 24 subarrays with at most 2 distinct elements.
    Constraints:
    1 ≤ arr.size() ≤ 2*104
    1 ≤ k ≤ 2*104
    1 ≤ arr[i] ≤ 109

---
+++ Subarrays With At Most K Distinct Integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        // code here
+        int ans=0;
+        int i=0;
+        int j=0;
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        while(j<n){
+            mp[arr[j]]++;
+            
+            while(i<j && mp.size()>k){
+                if(--mp[arr[i]]==0){
+                    mp.erase(arr[i]);
+                }
+                i++;
+            }
+            ans+=(j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
+// 1 -> [1]
