98154be1b8a5684fcb8b429323ab2450e7cbc722
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-02-21 14:17:18 +0530

    Create 2 Find H-Index.cpp
    You are given an array citations[], where each element citations[i] represents the number of citations received by the ith paper of a researcher. You have to calculate the researcher’s H-index.
    The H-index is defined as the maximum value H, such that the researcher has published at least H papers, and all those papers have citation value greater than or equal to H.
    
    Examples:
    
    Input: citations[] = [3, 0, 5, 3, 0]
    Output: 3
    Explanation: There are at least 3 papers with citation counts of 3, 5, and 3, all having citations greater than or equal to 3.
    Input: citations[] = [5, 1, 2, 4, 1]
    Output: 2
    Explanation: There are 3 papers (with citation counts of 5, 2, and 4) that have 2 or more citations. However, the H-Index cannot be 3 because there aren't 3 papers with 3 or more citations.
    Input: citations[] = [0, 0]
    Output: 0
    Explanation: The H-index is 0 because there are no papers with at least 1 citation.
    Constraints:
    1 ≤ citations.size() ≤ 106
    0 ≤ citations[i] ≤ 106
    
    Expected Complexities
    Company Tags
    NPCI
    Topic Tags
    Related Articles

---
+++ 2 Find H-Index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool check(vector<int>&arr,int value){
+        int cnt=0;
+        for(int i:arr){
+            if(i>=value)cnt++;
+        }
+        return cnt>=value;
+    }
+    int hIndex(vector<int>& arr) {
+        // code here
+        int low=0,high=*max_element(arr.begin(),arr.end());
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(arr,mid)){
+                ans=mid;
+                low=mid+1;
+            } else high=mid-1;
+        }
+        return ans;
+    }
+};
+
