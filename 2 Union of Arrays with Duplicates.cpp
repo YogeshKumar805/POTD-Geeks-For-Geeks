39bb0895e3d23ff0c7e8e7fe0c9d6305312cf9e6
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-02-23 10:03:50 +0530

    Create 2 Union of Arrays with Duplicates.cpp
    You are given two arrays a[] and b[], return the Union of both the arrays in any order.
    
    The Union of two arrays is a collection of all distinct elements present in either of the arrays. If an element appears more than once in one or both arrays, it should be included only once in the result.
    
    Note: Elements of a[] and b[] are not necessarily distinct.
    Note that, You can return the Union in any order but the driver code will print the result in sorted order only.
    
    Examples:
    
    Input: a[] = [1, 2, 3, 2, 1], b[] = [3, 2, 2, 3, 3, 2]
    Output: [1, 2, 3]
    Explanation: Union set of both the arrays will be 1, 2 and 3.
    Input: a[] = [1, 2, 3], b[] = [4, 5, 6] 
    Output: [1, 2, 3, 4, 5, 6]
    Explanation: Union set of both the arrays will be 1, 2, 3, 4, 5 and 6.
    Input: a[] = [1, 2, 1, 1, 2], b[] = [2, 2, 1, 2, 1] 
    Output: [1, 2]
    Explanation: Union set of both the arrays will be 1 and 2.
    Constraints:
    1 ≤ a.size(), b.size() ≤ 106
    0 ≤ a[i], b[i] ≤ 105
    
    Expected Complexities
    Company Tags

---
+++ 2 Union of Arrays with Duplicates.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        unordered_set<int>st(a.begin(),a.end());
+        st.insert(b.begin(),b.end());
+        return vector<int>(st.begin(),st.end());
+    }
+};
