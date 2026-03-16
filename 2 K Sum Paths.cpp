8698450123b828d3856281396f7e6a5b98c52c34
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-03-16 13:51:52 +0530

    Create 2 K Sum Paths.cpp
    Given the root of a binary tree and an integer k, determine the number of downward-only paths where the sum of the node values in the path equals k.
    
    Note: A path can start and end at any node within the tree but must always move downward (from parent to child).
    
    Examples:
    
    Input: root = [8, 4, 5, 3, 2, N, 2, 3, -2, N, 1], k = 7
    
    Output: 3
    Explanation: The following paths sum to k
     
    Input: root = [1, 2, 3], k = 3
    
    Output: 2 
    Explanation: The following paths sum to k
    
    Constraints:
    1 ≤ number of nodes ≤ 104
    -100 ≤ node value ≤ 100
    -109 ≤ k ≤ 109
    
    Expected Complexities
    Company Tags
    FlipkartAmazonWalmartNPCI
    Topic Tags
    Related Interview Experiences

---
+++ 2 K Sum Paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  private:
+  
+    int dfs(Node *root, int sum, int k, map<int,int>&mp)
+    {
+        if(!root) return 0;
+        sum += root->data;
+        int res = mp[sum - k];
+        
+        mp[sum]++;
+        if(root->left) res += dfs(root->left, sum, k, mp);
+        if(root->right) res += dfs(root->right, sum, k, mp);
+        mp[sum]--;
+        return res;
+    }
+    
+    
+  public:
+    // can maintain a map of the sums
+    int countAllPaths(Node *root, int k) {
+        map<int, int>mp;
+        mp[0] = 1;
+        
+        int sum = 0;
+        return dfs(root, sum, k, mp);
+    }
+};
