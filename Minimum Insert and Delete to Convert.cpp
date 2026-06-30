5a959f6b2bd5838a32f29f99717bfe594281c120
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-06-30 14:08:42 +0530

    Create Minimum Insert and Delete to Convert.cpp
    Given two arrays a[] and b[] of size n and m respectively, find the minimum number of insertions and deletions on the array a[], required to make both the arrays identical.
    
    Note: Array b[] is sorted and all its elements are distinct, operations can be performed at any index not necessarily at the end.
    
    Examples :
    
    Input: a[] = [1, 2, 5, 3, 1], b[] = [1, 3, 5]
    Output: 4
    Explanation:
    Delete 2 from a: a[] = [1, 5, 3, 1]
    Insert 3 after 1: a[] = [1, 3, 5, 3, 1]
    Delete the last two elements: a[] = [1, 3, 5]
    Total operations = 1 + 1 + 2 = 4.
    Input: a[] = [1, 4], b[] = [1, 4]
    Output : 0
    Explanation: Both the Arrays are already identical.
     Constraints:
    1 ≤ n, m ≤ 105
    1 ≤ a[i], b[i] ≤ 105
    
    Expected Complexities
    Company Tags
    GoogleCodenation
    Topic Tags
    Related Articles
    If you are facing any issue on this page. Please let us know.

---
+++ Minimum Insert and Delete to Convert.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int minInsAndDel(vector<int> &a, vector<int> &b) {
+        vector<int> P;
+        for(int x : a) if(binary_search(b.begin(),b.end(),x)) P.push_back(x);
+        
+        vector<int> lis;
+        for(int x : P){
+            auto it = lower_bound(lis.begin(),lis.end(),x);
+            if(it == lis.end()) lis.push_back(x);
+            else *it = x;
+        }
+        
+        return (a.size() - b.size()) + 2 * (b.size() - lis.size());
+    }
+};
