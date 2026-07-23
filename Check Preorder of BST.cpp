826acb0d87336bf012edeca59e2b387a12afb48a
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-07-23 17:01:37 +0530

    Create Check Preorder of BST.cpp
    Given an array arr[ ] consisting of distinct integers, check if the given array can represent preorder traversal of a BST.
    
    Examples :
    
    Input: arr[] = [2, 4, 3]
    Output: true
    Explaination: Given arr[] can represent preorder traversal of following BST:
     
    Input: arr[] = [2, 4, 1]
    Output: false
    Explaination: Given arr[] cannot represent preorder traversal of a BST.
    Constraints:
    1 ≤ arr.size() ≤ 105
    0 ≤ arr[i] ≤ 105
    
    Expected Complexities
    Company Tags
    Morgan StanleyMicrosoftAdobe
    Topic Tags
    Related Articles

---
+++ Check Preorder of BST.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    bool canRepresentBST(vector<int> &arr) {
+        stack<int> st;
+
+        int lowerBound = INT_MIN;
+
+        for (int value : arr) {
+
+            if (value < lowerBound)
+                return false;
+
+            while (!st.empty() && value > st.top()) {
+
+                lowerBound = st.top();
+
+                st.pop();
+            }
+
+            st.push(value);
+        }
+
+        return true;
+    }
+};
