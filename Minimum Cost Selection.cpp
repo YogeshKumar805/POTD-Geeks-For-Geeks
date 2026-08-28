b48c648731ec46635f42e45f1ca9c5e52f9343cd
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-08-28 20:18:37 +0530

    Create Minimum Cost Selection.cpp
    Given an n × 3 matrix mat[][], where each row represents the costs of three available choices at a shop, select exactly one choice from each row such that the same choice is not selected in two adjacent rows.
    
    Return the minimum total cost required.
    
    Examples:
    
    Input: mat[][] = [[1, 50, 50], [50, 50, 50], [1, 50, 50]]
    Output: 52
    Explanation: One optimal selection is- Row 1: Choice 1 (Cost = 1), Row 2: Choice 2 (Cost = 50), Row 3: Choice 1 (Cost = 1)
    Total cost = 1 + 50 + 1 = 52.
    Input: mat[][] = [[1, 4, 1], [3, 2, 2], [3, 2, 3]]
    Output: 5
    Explanation: One optimal selection is- Row 1: Choice 1 (Cost = 1), Row 2: Choice 2 (Cost = 2), Row 3: Choice 3 (Cost = 2)
    Total cost = 1 + 2 + 2 = 5.
    Constraints:
    
    1 ≤ n ≤ 105
    3 ≤ mat[0].size() ≤ 3
    1 ≤ mat[i][j] ≤ 100
    mat.rows = n
    Expected Complexities
    Topic Tags
    Related Articles

---
+++ Minimum Cost Selection.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int minCost(vector<vector<int>>& mat) {
+        int n = mat.size();
+        
+        for(int i = 1; i < n; i++){
+            mat[i][0] += min(mat[i-1][1], mat[i-1][2]);
+            mat[i][1] += min(mat[i-1][0], mat[i-1][2]);
+            mat[i][2] += min(mat[i-1][0], mat[i-1][1]);
+        }
+        return *min_element(mat[n-1].begin(), mat[n-1].end());
+    }
+};
