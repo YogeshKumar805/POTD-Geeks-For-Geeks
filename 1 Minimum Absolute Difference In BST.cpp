4c39e367b59aebe6c2080e54eccbb54223bb3570
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-09-18 18:59:22 +0530

    Create 1 Minimum Absolute Difference In BST.cpp
    Given the root of a Binary Search Tree (BST) containing n (n > 1) nodes, find the minimum absolute difference between the values of any two different nodes in the tree.
    
    Return the minimum absolute difference.
    
    Examples:
    
    Input: root[] = [50, 30, 70, 20, N, 60, 80]
    420046990
    Output: 10
    Explanation: There are no two nodes whose absolute difference is smaller than 10.
    Input: root[] = [60, 30, 90, 10]
    2056957925
    Output: 20
    Explanation: There are no two nodes whose absolute difference is smaller than 20.
    Constraints:
    
    2 ≤ size of binary tree ≤ 105
    0 ≤ node.data ≤ 106
    Expected Complexities
    Topic Tags
    Related Articles

---
+++ 1 Minimum Absolute Difference In BST.cpp
@@ -0,0 +1,19 @@
+class Solution {
+	public:
+	int ans;
+	int absDiff(Node *root) {
+		ans=1e9;
+		int prev=1e7;
+		dfs(root,prev);
+		return ans;
+	}
+	
+	void dfs(Node* root,int &prev){
+	    if(root==NULL) return;
+	    
+	    dfs(root->left,prev);
+	    ans=min(ans,abs(root->data-prev));
+	    prev=root->data;
+	    dfs(root->right,prev);
+	}
+};
